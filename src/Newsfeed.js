f4af35e02c75c8fceb658f9dc03627aa9116e51f
adambenaceur <benaceur.adam@gmail.com>
2022-03-27 13:56:39 -0600

    added market news + popular list section

--- src/Newsfeed.js
+++ src/Newsfeed.js
@@ -2,6 +2,20 @@ import React from 'react'
 import './Newsfeed.css'
 import LineGraph from './LineGraph'
 import TimeLine from './TimeLine'
+import { Avatar, Chip } from '@material-ui/core'
+
+const popularTopics = [
+  'Technology',
+  'Top Movies',
+  'Upcoming Earnings',
+  'Crypto',
+  'Cannabis',
+  'Healthcare Supplies',
+  'Index ETFs',
+  'Technology',
+  'China',
+  'Pharma'
+];
 
 function Newsfeed() {
   return (
@@ -15,6 +29,33 @@ function Newsfeed() {
             <div className='newsfeed__chart'>
               <LineGraph/>
               <TimeLine />
+            </div>
+            <div className='newsfeed__buying__section'>
+              <h2>Buying Power</h2>
+              <h2>4.11</h2>
+            </div>
+            <div className='newsfeed__market__section'>
+              <div className='newsfeed__market__box'>
+                <p> Markets closed</p>
+              </div>
+            </div>
+            <div className='newsfeed__popular__lists__section'>
+            <div className='newsfeed__popular__lists__intro'>
+              <h1>Popular list</h1>
+              <p> Show more</p>
+            </div>
+            <div className='newsfeed__popular__lists__badges'>
+              {popularTopics.map((topic) => (
+                <Chip
+                  className='topic__badge'
+                  variant='outlined'
+                  label={topic}
+                  avatar={<Avatar
+                    src={''}
+                  />}
+                />
+              ))}
+             </div>
             </div>
           </div>
 
